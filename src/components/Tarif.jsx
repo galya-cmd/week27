8209e19371a578b8f8aa577244b3ae28ab34acb9
galya-cmd <galya1985@bk.ru>
2021-12-18 00:20:09 +0300

    dsfg

---
+++ src/components/Tarif.jsx
@@ -0,0 +1,15 @@
+import '.Tarif.css';
+
+function Tarif() {
+    return(
+    
+      <div className="cart">
+      <div className="tariff-name">Безлимитный 300</div>
+      <div className="priсe">300руб.</div>
+      <div className="speed">до 10 Мбит/сек</div>
+      <div className="text">Обьем включенного трафика не ограничен</div> 
+      </div>
+      );
+}
+
+export default Tarif;
