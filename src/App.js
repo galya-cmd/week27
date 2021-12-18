3e698825623680f7abf236e709d5570f70e8da7c
galya-cmd <galya1985@bk.ru>
2021-12-18 14:38:53 +0300

    массив с тарифами

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 
-import logo from './logo.svg';
+
 import './App.css';
 import Tarif from './components/Tarif.jsx';
 import './components/Tarif.css';
 
+const tarifs = [{name : "Безлимитный 300", price : "300 руб./мес", speed :"до 10 Мбит/сек", text :"Обьем включенного тарифа не ограничен"},
+{ name :"Безлимитный 450", price:"450 руб./мес" ,speed :"до 50 Мбит/сек", text:"Обьем включенного тарифа не ограничен"},
+{name:"Безлимитный 550", price:"550 руб./мес" ,speed:"до 200 Мбит/сек" ,text:"Обьем включенного тарифа не ограничен"},
+{name:"Безлимитный 1000", price:"1000 руб./мес", speed:"до 10 Мбит/сек", text:"Обьем включенного тарифа не ограничен"}
+]
+
 function App () {
   return (
+    <>
+
     <div className="App">
-      <Tarif></Tarif>
+      {
+        tarifs.map((tarif)) => <Tarif name={tarif.name} price= {tarif.price} speed = {tarif.speed} text = {tarif.text}></Tarif>
+      }
+      
       
     </div>
+    </>
   );
 
 }
