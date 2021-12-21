dd45ff77bf299029e5bf5a0037d773d914cde39f
galya-cmd <galya1985@bk.ru>
2021-12-21 15:09:39 +0300

    добавила цвета

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 import Tarif from './components/Tarif.jsx';
 import './components/Tarif.css';
 
-const tarifs = [{name : "Безлимитный 300", price : "300 руб./мес", speed :"до 10 Мбит/сек", text :"Обьем включенного тарифа не ограничен"},
-{ name :"Безлимитный 450", price:"450 руб./мес" ,speed :"до 50 Мбит/сек", text:"Обьем включенного тарифа не ограничен"},
-{name:"Безлимитный 550", price:"550 руб./мес" ,speed:"до 200 Мбит/сек" ,text:"Обьем включенного тарифа не ограничен"},
-{name:"Безлимитный 1000", price:"1000 руб./мес", speed:"до 10 Мбит/сек", text:"Обьем включенного тарифа не ограничен"}
+const tarifs = [{name : "Безлимитный 300", color : 'grren', price : "300 руб./мес", speed :"до 10 Мбит/сек", text :"Обьем включенного тарифа не ограничен"},
+{ name :"Безлимитный 450",color : 'grey', price:"450 руб./мес" ,speed :"до 50 Мбит/сек", text:"Обьем включенного тарифа не ограничен"},
+{name:"Безлимитный 550", color : 'blue', price:"550 руб./мес" ,speed:"до 200 Мбит/сек" ,text:"Обьем включенного тарифа не ограничен"},
+{name:"Безлимитный 1000",color : 'red', price:"1000 руб./мес", speed:"до 10 Мбит/сек", text:"Обьем включенного тарифа не ограничен"}
 ]
 
 function App () {
@@ -16,7 +16,7 @@ function App () {
 
     <div className="App">
       {
-        tarifs.map((tarif) => <Tarif name={tarif.name} price= {tarif.price} speed = {tarif.speed} text = {tarif.text}></Tarif>
+        tarifs.map((tarif) => <Tarif name={tarif.name} color={tarif.color} price= {tarif.price} speed = {tarif.speed} text = {tarif.text}></Tarif>
         )
       }
       
--- src/components/Tarif.jsx
+++ src/components/Tarif.jsx
@@ -3,14 +3,16 @@ import './Tarif.css';
 
 class Tarif extends React.Component {
   render(){
-  const {name,price,speed,text} = this.props;
+  const {name,color,price,speed,text} = this.props;
   
   
   
     return(
     
       <div className="cart">
+      
       <div className="tariff-name">{name}</div>
+      <div>{color}</div>
       <div className="priсe">{price}</div>
       <div className="speed">{speed}</div>
       <div className="text">{text}</div> 
